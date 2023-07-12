776ea546b3575c4be346543554994dbddca3e8c4
andreggalvao <andreggalvao@gmail.com>
2023-07-12 20:03:35 -0300

    up

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 from pydantic import BaseModel
 import warnings
 from datetime import timedelta
+
 # Define o modo de filtragem de warnings para "ignore"
 warnings.filterwarnings("ignore")
 warnings.filterwarnings("ignore", message=".*If you are loading a serialized model.*")
@@ -13,6 +14,7 @@ warnings.filterwarnings("ignore", message=".*If you are loading a serialized mod
 
 app = FastAPI()
 
+# Defining information receiving structure
 class TransactionData(BaseModel):
     transaction_id: int
     merchant_id: int
@@ -22,19 +24,17 @@ class TransactionData(BaseModel):
     transaction_amount: float
     device_id: int
 
-# Criação do DataFrame vazio para armazenar as transações
+# Creating an empty DataFrame to store transactions
 historico = pd.DataFrame(columns=['transaction_id', 'merchant_id', 'user_id', 'card_number',
                            'transaction_date', 'transaction_amount', 'device_id'])
 
-@app.get("/vitoria")
-async def root():
-    return {"message": "A vitória é o amor da minha vida, a esposa mais linda deste universo"}
 
+# Defining the post function
 @app.post('/modelo')
 def previsao_modelo(transaction: TransactionData):
     global historico
 
-    # Cria um dicionário com os dados da transação atual
+    # Create a dictionary with the current transaction data
     data = {
         'transaction_id': [transaction.transaction_id],
         'merchant_id': [transaction.merchant_id],
@@ -45,50 +45,44 @@ def previsao_modelo(transaction: TransactionData):
         'device_id': [transaction.device_id],
     }
 
-    # Adiciona a transação atual ao DataFrame
+    # Add the current transaction to the DataFrame
     ultima_entrada_dados = pd.DataFrame(data, index=[0])
     historico = pd.concat([historico, ultima_entrada_dados], ignore_index=True)
 
-    #modelo = pickle.load(open('modelo_treinado.pkl', 'rb'))
-    modelo = pd.read_pickle('modelo_treinado.pkl')
-
-
+    
+    # Setting reject flag
     deve_ser_rejeitada = False
 
-
-    # Antifraud Requirements
+    # Elaborating a specific dataframe with all the transactions of the last user_id that performed the transaction
     investigacao_cliente = historico.loc[historico['user_id'] == ultima_entrada_dados.iloc[0]['user_id']]
     investigacao_cliente.sort_values(by = 'transaction_date')
     investigacao_cliente.reset_index(inplace = True, drop = True )
     investigacao_cliente['transaction_date'] = pd.to_datetime(investigacao_cliente['transaction_date'])
 
-    #Reject transaction if a user is trying too many transactions in a row
-
+    ## Reject transaction if a user is trying too many transactions in a row
     maximo_de_transacoes_seguidas = 3
-    maximo_duracao = timedelta(hours=0.5) #tempo míninmo entre lote de transações
+    maximo_duracao = timedelta(hours=0.5) 
 
     mensagem_frequencia = 'A frequência de transação está dentro do previsto'
     if len(investigacao_cliente) >= maximo_de_transacoes_seguidas:
         
-        # Loop para percorrer as transações
+        # Loop through transactions
         for i in range(len(investigacao_cliente) - maximo_de_transacoes_seguidas + 1):
-            # Seleciona as transações seguidas
+            # Select the transactions in a row
             transacoes_seguidas = investigacao_cliente.iloc[i:i+maximo_de_transacoes_seguidas]
             
             # Calcula a duração entre a primeira e a última transação
             duracao = transacoes_seguidas.iloc[-1]['transaction_date'] - transacoes_seguidas.iloc[0]['transaction_date']
             
-            # Verifica se a duração é menor ou igual a 3 horas
+            # Check if the duration is less than or equal to the established limit
             if duracao <= maximo_duracao:
                 mensagem_frequencia = "Comportamento suspeito: 3 ou mais transações seguidas em um período menor de 30 minutos"
                 deve_ser_rejeitada = True
-                # Interrompe o loop, já que o comportamento suspeito foi detectado
                 break
         else:
-            # Se não houver comportamento suspeito, exibe a mensagem de transações suficientes
-           mensagem_frequencia = "This client have 3 or more transactions"
+           mensagem_frequencia = "Este cliente tem 3 ou mais transações não suspeitas"
 
-
+    ## Reject transaction if above a threshold
     mensagem_amount = 'Valor de transação dentro do estabelecido'
     maximo_valor_de_transacao = 10000
     if transaction.transaction_amount > maximo_valor_de_transacao:
@@ -96,7 +90,7 @@ def previsao_modelo(transaction: TransactionData):
         deve_ser_rejeitada = True
 
 
-
+    ## Reject transaction if there is a chargeback in the past
     mensagem_cbk = 'Usuário não possui histórico de chargeback'
     cbk_historico = pd.read_excel('cbk_stats.xlsx')
     try:
@@ -108,11 +102,12 @@ def previsao_modelo(transaction: TransactionData):
         pass
 
     
-    
-        
+    # Receiving the Machine Learning Model
+    modelo = pd.read_pickle('modelo_treinado.pkl')   
+    # Let's pass a copy of the original information to our dataset that will be treated 
+    dados = ultima_entrada_dados.copy() 
 
-    dados = ultima_entrada_dados.copy() #Vamos passar uma copia das informações originais para o nosso dataset que será tratado
-    # adicionando todas as modificações do DF
+    # Redoing all the modifications that were made to the model
     dados['transaction_date'] = pd.to_datetime(dados['transaction_date'])
     dados['dia'] = dados['transaction_date'].dt.day
     dados['mês'] = dados['transaction_date'].dt.month
@@ -124,19 +119,18 @@ def previsao_modelo(transaction: TransactionData):
     dados['card_number'] = pd.to_numeric(dados['card_number'])
     dados.drop(columns = 'device_id', inplace = True)
 
-
-
-
+    ## Reject transaction if there is model not approve
     mensagem_modelo = 'A transação está aprovada pelo nosso modelo de crédito'
     if modelo.predict(dados)[0] == 1:
         mensagem_modelo = 'A transação está rejeitada pelo nosso modelo de crédito'
 
-
+    ## Reject transaction if any anti-fraud feature is enabled
     mensagem_final = 'A transação está aprovada'
     if deve_ser_rejeitada or modelo.predict(dados)[0] == 1:
         mensagem_final = 'A transação deve ser rejeitada'
     else:
         pass
+    
     
     return {print('A "transaction_id":{}'.format(transaction.transaction_id)),
             print(mensagem_frequencia),
